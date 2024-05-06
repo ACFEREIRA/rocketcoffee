e8d8486b97ecb928499c4296d393e100f23e33df
Ana Clara <annacllara98@gmail.com>
2024-05-06 12:30:30 -0300

    add styles mobile

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-let open = true;
-
-const menu = document.querySelector(".menu");
-const menuClose = document.querySelector(".menu_mobile i");
-const menuMobile = document.querySelector(".menu_mobile");
-
-menuMobile.addEventListener("click", () => {
-  menuClose.classList.toggle("close", open);
-  menu.classList.toggle("open", open);
-  open = !open;
-});
+function toggleMenu() {
+  var navMenu = document.getElementById('nav_menu');
+  navMenu.classList.toggle('open');
+}
