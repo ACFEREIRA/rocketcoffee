6e53ddc8935e9727a13a7592da7002b56133d829
Ana Clara <annacllara98@gmail.com>
2024-05-08 11:43:42 -0300

    script update

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-function toggleMenu() {
-  var navMenu = document.getElementById('nav_menu');
-  navMenu.classList.toggle('open');
+// function toggleMenu() {
+//   var navMenu = document.getElementById('nav_menu');
+//   navMenu.classList.toggle('open');
+// }
+
+function toggleMenu(event) {
+  var navMenu = document.getElementById('nav_menu'); /* Menu Mobile */
+  var menuCloseButton = document.querySelector('.menu_close_button'); /* Botão fechar Menu Mobile */
+  var menuMobileButton = document.querySelector('.menu_mobile'); /* Botão hamburguer abrir Menu Mobile */
+
+  if (event.target === menuMobileButton) {
+    navMenu.classList.toggle('open');
+  }
+
+  else if (event.target === menuCloseButton) {
+    navMenu.classList.remove('open');
+  }
 }
